45b7f015feeccd5cf2c108383bc020266b9d0828
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-19 09:29:47 -0700

    Display existing orders

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,4 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => {
-        console.log(response)
-        response.json()
-      })
+    .then(response => response.json())
 }
