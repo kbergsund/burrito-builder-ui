f6d6cab11d7cfd38791ef2e798fc1d8d15c42827
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-19 10:59:13 -0700

    Write homepage on load tests

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -10,8 +10,25 @@ describe('Homepage tests', () => {
       })
   })
 
-  it('should display title, form & any existing orders', () => {
-    // cy.visit('http://localhost:3000/')
-    cy.get('h1').contains('Burrito Builder')
+  it('should display header containing title & form on load', () => {
+    cy.get('main').get('header').get('h1').contains('Burrito Builder')
+
+    .get('form')
+    .get('input').invoke('attr', 'placeholder').should('contain','Name')
+    .get('[name=beans]')
+    .get(':nth-child(1)').contains('beans')
+    .get(':nth-child(13)').contains('sour cream')
+    .get('p').contains('Order: Nothing selected')
+    .get('button').contains('Submit Order')
+  })
+
+  it('should display all existing orders on load', () => {
+    cy.get('section')
+    .get(':nth-child(1)').get('h3').contains('Pat')
+    .get('.ingredient-list > :nth-child(5)').contains('jalapeno')
+    .get(':nth-child(2)').get('h3').contains('Sam')
+    .get('.ingredient-list > :nth-child(6)').contains('jalapeno')
+    .get(':nth-child(3)').get('h3').contains('Alex')
+    .get('.ingredient-list > :nth-child(5)').contains('queso fresco')
   })
 })
